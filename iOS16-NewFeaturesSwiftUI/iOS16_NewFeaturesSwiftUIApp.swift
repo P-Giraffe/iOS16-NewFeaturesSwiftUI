89c81d968ce9fb5a9b5230ce0b16b98e61183e06
Cityhunter <johan.guenaoui@gmail.com>
2022-08-27 17:30:11 +0200

    Initial Commit

---
+++ iOS16-NewFeaturesSwiftUI/iOS16_NewFeaturesSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS16_NewFeaturesSwiftUIApp.swift
+//  iOS16-NewFeaturesSwiftUI
+//
+//  Created by Johan Guenaoui on 27/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct iOS16_NewFeaturesSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
