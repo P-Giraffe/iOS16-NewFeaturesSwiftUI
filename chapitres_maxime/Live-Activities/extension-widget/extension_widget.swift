208e42bcd9710467b12b81b11ab575eb6d053aa6
Maxime Britto <maxime@purplegiraffe.fr>
2022-09-19 12:11:40 +0200

    Création des petites vues de la dynamic island

--- chapitres_maxime/Live-Activities/extension-widget/extension_widget.swift
+++ chapitres_maxime/Live-Activities/extension-widget/extension_widget.swift
@@ -13,20 +13,47 @@ import ActivityKit
 struct LearnTimerActivityWidget: Widget {
     var body: some WidgetConfiguration {
         ActivityConfiguration(for:LearnTimerAttributes.self) { context in
-            VStack {
-                Text(context.attributes.courseName)
-                    .font(.headline)
-                    .multilineTextAlignment(.center)
-                HStack {
-                    Text(context.state.plannedDuration.lowerBound, style: .time)
-                    ProgressView(timerInterval: context.state.plannedDuration, countsDown: false)
-                    Text(context.state.plannedDuration.upperBound, style: .time)
-                }
-            }.padding()
+            LiveActivityView(context: context)
         } dynamicIsland: { context in
-            <#code#>
+            DynamicIsland {
+                <#code#>
+            } compactLeading: {
+                Image("logo")
+                    .resizable()
+                    .frame(width: 25, height: 25)
+            } compactTrailing: {
+                Text(timerInterval: context.state.plannedDuration, countsDown: true)
+                    .frame(width: 40)
+                    .font(.caption2)
+                    .multilineTextAlignment(.center)
+            } minimal: {
+                VStack(alignment: .center) {
+                    Image(systemName: "timer")
+                    Text(timerInterval: context.state.plannedDuration, countsDown: true)
+                        .multilineTextAlignment(.center)
+                        .monospacedDigit()
+                        .font(.caption2)
+                }
+            }
+
         }
 
+    }
+}
+
+struct LiveActivityView : View {
+    let context:ActivityViewContext<LearnTimerAttributes>
+    var body: some View {
+        VStack {
+            Text(context.attributes.courseName)
+                .font(.headline)
+                .multilineTextAlignment(.center)
+            HStack {
+                Text(context.state.plannedDuration.lowerBound, style: .time)
+                ProgressView(timerInterval: context.state.plannedDuration, countsDown: false)
+                Text(context.state.plannedDuration.upperBound, style: .time)
+            }
+        }.padding()
     }
 }
 
