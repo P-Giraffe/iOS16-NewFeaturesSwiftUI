a875bd4c5f35a3780d37509f7b10917c0054e477
Maxime Britto <maxime@purplegiraffe.fr>
2022-09-19 11:58:27 +0200

    Création de la vue Live Activity

--- chapitres_maxime/Live-Activities/extension-widget/extension_widget.swift
+++ chapitres_maxime/Live-Activities/extension-widget/extension_widget.swift
@@ -7,61 +7,26 @@
 
 import WidgetKit
 import SwiftUI
-
-struct Provider: TimelineProvider {
-    func placeholder(in context: Context) -> SimpleEntry {
-        SimpleEntry(date: Date())
-    }
-
-    func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> ()) {
-        let entry = SimpleEntry(date: Date())
-        completion(entry)
-    }
-
-    func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
-        var entries: [SimpleEntry] = []
-
-        // Generate a timeline consisting of five entries an hour apart, starting from the current date.
-        let currentDate = Date()
-        for hourOffset in 0 ..< 5 {
-            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
-            let entry = SimpleEntry(date: entryDate)
-            entries.append(entry)
-        }
-
-        let timeline = Timeline(entries: entries, policy: .atEnd)
-        completion(timeline)
-    }
-}
-
-struct SimpleEntry: TimelineEntry {
-    let date: Date
-}
-
-struct extension_widgetEntryView : View {
-    var entry: Provider.Entry
-
-    var body: some View {
-        Text(entry.date, style: .time)
-    }
-}
+import ActivityKit
 
 @main
-struct extension_widget: Widget {
-    let kind: String = "extension_widget"
-
+struct LearnTimerActivityWidget: Widget {
     var body: some WidgetConfiguration {
-        StaticConfiguration(kind: kind, provider: Provider()) { entry in
-            extension_widgetEntryView(entry: entry)
+        ActivityConfiguration(for:LearnTimerAttributes.self) { context in
+            VStack {
+                Text(context.attributes.courseName)
+                    .font(.headline)
+                    .multilineTextAlignment(.center)
+                HStack {
+                    Text(context.state.plannedDuration.lowerBound, style: .time)
+                    ProgressView(timerInterval: context.state.plannedDuration, countsDown: false)
+                    Text(context.state.plannedDuration.upperBound, style: .time)
+                }
+            }.padding()
+        } dynamicIsland: { context in
+            <#code#>
         }
-        .configurationDisplayName("My Widget")
-        .description("This is an example widget.")
+
     }
 }
 
-struct extension_widget_Previews: PreviewProvider {
-    static var previews: some View {
-        extension_widgetEntryView(entry: SimpleEntry(date: Date()))
-            .previewContext(WidgetPreviewContext(family: .systemSmall))
-    }
-}
