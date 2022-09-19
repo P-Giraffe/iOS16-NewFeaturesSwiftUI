db2f3cdb83f0f1445bf03c3304bd9ad38d3b691e
Maxime Britto <maxime@purplegiraffe.fr>
2022-09-19 12:33:51 +0200

    Démarrer une activité depuis votre app

--- chapitres_maxime/Live-Activities/Live-Activities/ContentView.swift
+++ chapitres_maxime/Live-Activities/Live-Activities/ContentView.swift
@@ -6,16 +6,39 @@
 //
 
 import SwiftUI
+import ActivityKit
 
 struct ContentView: View {
+    @State var status:String = ""
+    @State var currentActivity: Activity<LearnTimerAttributes>?
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Image("logo")
+                .resizable()
+                .frame(width: 100, height: 100)
+            Text(status)
+            Button("Démarrer le minuteur") {
+                startLiveActivity()
+            }
         }
         .padding()
+    }
+    
+    func startLiveActivity() {
+        let startDate = Date.now;
+        guard let endDate = Calendar.current.date(byAdding: .minute, value: 1, to: startDate) else {
+            return
+        }
+        let interval = startDate...endDate
+        let attributes = LearnTimerAttributes(courseName: "Les nouveautés de iOS 16")
+        let state = LearnTimerAttributes.LearnTimerStatus(plannedDuration: interval)
+        
+        do {
+            currentActivity = try Activity.request(attributes: attributes, contentState: state)
+            status = "Apprentissage débuté"
+        } catch (let error) {
+            status = "Erreur : \(error.localizedDescription)"
+        }
     }
 }
 
